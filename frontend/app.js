c8d1de2cced490cfdd173fd0600a5eeb4470fe9d
amna0488 <amnasalahudin123@gmail.com>
2023-12-04 22:32:53 +0500

    added doctor db and add doctor feature

--- frontend/app.js
+++ frontend/app.js
@@ -1,50 +1,61 @@
 'use strict';
 
-
-
 const express = require('express');
+const bodyParser = require('body-parser');
 require('dotenv').config();
+const axios = require('axios');
+
 // Constants
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
-const axios = require('axios');
-
 // App
 const app = express();
 app.use(express.static('public'));
+app.use(bodyParser.json()); // for parsing application/json
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html'); // Adjust the path to your HTML file
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 app.get('/hello', (req, res) => {
   res.send('Hello world\n');
 });
 
-// Define your API endpoints and routes here
+// Proxy GET requests to Flask server
 app.get('/api/doctors', async (req, res) => {
-  let url  = process.env.DOCTORS_SERVICE_URL;
+  let url = process.env.DOCTORS_SERVICE_URL;
   try {
-    const response = await axios.get(String(`http://${url}/doctors`));
-    const doctors = response.data;
-    res.json(doctors);
+    const response = await axios.get(`http://${url}/doctors`);
+    res.json(response.data);
   } catch (error) {
     console.error('Error fetching doctors:', error);
     res.status(500).json({ error: 'Could not fetch doctors' });
   }
 });
 
-app.get('/api/appointments', async (req, res) => {
-  let url  = process.env.APPOINTMENTS_SERVICE_URL;
+// POST requests to Flask server
+app.post('/api/doctors', async (req, res) => {
+  let url = process.env.DOCTORS_SERVICE_URL;
   try {
-    const response = await axios.get(String(`http://${url}/appointments`));
-    const appointments = response.data;
-    res.json(appointments);
+    const response = await axios.post(`http://${url}/doctors`, req.body);
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error adding doctor:', error);
+    res.status(500).json({ error: 'Could not add doctor' });
+  }
+});
+
+app.get('/api/appointments', async (req, res) => {
+  let url = process.env.APPOINTMENTS_SERVICE_URL;
+  try {
+    const response = await axios.get(`http://${url}/appointments`);
+    res.json(response.data);
   } catch (error) {
     console.error('Error fetching appointments:', error);
     res.status(500).json({ error: 'Could not fetch appointments' });
   }
 });
 
-app.listen(PORT, HOST); 
+app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
