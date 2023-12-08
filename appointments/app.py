715306766ed21492fbda5a82068fe65415addb4b
amna0488 <amnasalahudin123@gmail.com>
2023-12-09 00:08:30 +0500

    seprate pipelines for each service

--- appointments/app.py
+++ appointments/app.py
@@ -67,7 +67,7 @@ def getAppointment(id):
     else:
         return jsonify({"error": "Appointment not found"}), 404
     
-   #edit
+  
 @app.route('/health', methods=['GET'])
 def health_check():
     return 'OK', 200
